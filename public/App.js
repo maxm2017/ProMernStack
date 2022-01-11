5a5cf2d6ada30c339e9656dee9f1a3978363f535
Max Mercel <mmercel8@gmail.com>
2022-01-11 20:52:42 +0000

    Installed express and added server file, seperated App.jsx from index, compiled to js with babel

---
+++ public/App.js
@@ -0,0 +1,6 @@
+/*#__PURE__*/
+React.createElement("script", {
+  type: "text/babel"
+}, "const element = (", /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", null, "Hello World!")), "); ReactDOM.render(element, document.getElementById('contents'));");
