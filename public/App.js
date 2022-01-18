40c9f288887848cd222798f68b2d579bbcb7108c
Max Mercel <mmercel8@gmail.com>
2022-01-18 16:50:28 +0000

    Added IssueRow class which is used inside IssueTable

--- public/App.js
+++ public/App.js
@@ -14,7 +14,27 @@ class IssueFilter extends React.Component {
 
 class IssueTable extends React.Component {
   render() {
-    return /*#__PURE__*/React.createElement("div", null, " Placeholder for issue table ");
+    const rowStyle = {
+      border: "1px solid silver",
+      padding: 4
+    };
+    return /*#__PURE__*/React.createElement("table", {
+      style: {
+        borderCollapse: "collapse"
+      }
+    }, /*#__PURE__*/React.createElement("thead", null, /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("th", {
+      style: rowStyle
+    }, "ID"), /*#__PURE__*/React.createElement("th", {
+      style: rowStyle
+    }, "Title"))), /*#__PURE__*/React.createElement("tbody", null, /*#__PURE__*/React.createElement(IssueRow, {
+      rowStyle: rowStyle,
+      issue_id: 1,
+      issue_title: "Error in console when clicking Add"
+    }), /*#__PURE__*/React.createElement(IssueRow, {
+      rowStyle: rowStyle,
+      issue_id: 2,
+      issue_title: "Missing bottom border on panel"
+    })));
   }
 
 }
@@ -22,6 +42,18 @@ class IssueTable extends React.Component {
 class IssueAdd extends React.Component {
   render() {
     return /*#__PURE__*/React.createElement("div", null, " Placeholder for issue add ");
+  }
+
+}
+
+class IssueRow extends React.Component {
+  render() {
+    const style = this.props.rowStyle;
+    return /*#__PURE__*/React.createElement("tr", null, /*#__PURE__*/React.createElement("td", {
+      style: style
+    }, this.props.issue_id), /*#__PURE__*/React.createElement("td", {
+      style: style
+    }, this.props.issue_title));
   }
 
 }
--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,21 @@ class IssueFilter extends React.Component{
 class IssueTable extends React.Component{
     render()
     {
+        const rowStyle = {border: "1px solid silver" , padding: 4}
         return (
-            <div> Placeholder for issue table </div>
+            <table style={{borderCollapse: "collapse"}}>
+                <thead>
+                    <tr>
+                        <th style = {rowStyle}>ID</th>
+                        <th style = {rowStyle}>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow rowStyle = {rowStyle} issue_id={1} issue_title="Error in console when clicking Add" /> 
+                    <IssueRow rowStyle = {rowStyle} issue_id={2} issue_title="Missing bottom border on panel" /> 
+
+                </tbody>
+            </table>
         )
     }
 }
@@ -40,5 +53,17 @@ class IssueAdd extends React.Component{
     }
 }
 
+class IssueRow extends React.Component {
+    render()
+    {
+        const style = this.props.rowStyle;
+        return(
+            <tr>
+                <td style = {style}>{this.props.issue_id}</td>
+                <td style = {style}>{this.props.issue_title}</td>
+            </tr>
+        )
+    }
+}
 const element = <IssueList />
 ReactDOM.render(element, document.getElementById('contents'));
