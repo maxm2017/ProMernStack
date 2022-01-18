e7bc44f93773f8b5cdf8f6c4a4db973540315669
Max Mercel <mmercel8@gmail.com>
2022-01-18 16:18:54 +0000

    Added Helloworld component

--- public/App.js
+++ public/App.js
@@ -1,4 +1,14 @@
-const element = /*#__PURE__*/React.createElement("div", {
-  title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World! New!"));
+class HelloWord extends React.Component {
+  render() {
+    const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+    const helloContinents = Array.from(continents, c => `Hello ${c}`);
+    const message = helloContinents.join(' ');
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+      title: "Outer div"
+    }, /*#__PURE__*/React.createElement("h1", null, message)), /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, " Second Div ")));
+  }
+
+}
+
+const element = /*#__PURE__*/React.createElement(HelloWord, null);
 ReactDOM.render(element, document.getElementById('contents'));
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-const element = (
-<div title="Outer div">
-<h1>Hello World! New!</h1>
-</div>
-);
+class HelloWord extends React.Component 
+{
+    render()
+    {
+        const continents = ['Africa','America','Asia', 'Australia', 'Europe'];
+        const helloContinents = Array.from(continents, c=> `Hello ${c}`);
+        const message = helloContinents.join(' ');
+        return(
+
+            <React.Fragment>
+                <div title = "Outer div">
+                    <h1>{message}</h1>
+                </div>
+
+                <div>
+                    <h2> Second Div </h2>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+const element = <HelloWord />;
 
 ReactDOM.render(element, document.getElementById('contents'));
