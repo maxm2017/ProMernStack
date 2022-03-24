8c347116031ce59bd889f435084921e8eda21c97
Max Mercel <mmercel8@gmail.com>
2022-03-24 20:40:55 +0000

    Added GraphQL schema and code to initialise server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,41 @@
+
+
 const express = require('express');
+const { AppolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+function setAboutMessage(_,{ message }){
+    return aboutMessage = message;
+}
+
+const typeDefs = `
+    type Query {
+        about:String!
+    }
+    type Mutation {
+        setAboutMessage(message: String!):String
+    } `;
+
+    const resolvers = {
+        Query: {
+            about: () => aboutMessage,
+        },
+        Mutation: {
+            setAboutMessage,
+        },
+    };
+
+const server = new AppolloServer({
+    typeDefs,
+    resolvers,
+});
+
 const app = express();
 app.use(express.static('public'));
+
+server.applyMiddleware({app, path: '/graphql'});
+
 app.listen(3000, function () {
  console.log('App started on port 3000');
 });
